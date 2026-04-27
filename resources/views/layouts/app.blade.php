31b16f5857e528bd0835170dd7f6700a2745532a
Dannys02 <webdannys@gmail.com>
2026-04-27 14:20:24 +0700

    penyesuaian kecil dan perbaiki logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta rel="icon" href="{{ asset('Logo.png') }}" type="image/png">
-    <title>Admin Kafe - Aplikasi Kasir</title>
+    <link rel="icon" href="{{ asset('Logo.png') }}" type="image/png">
+    <title>Admin Kafe | Aplikasi Kasir</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
